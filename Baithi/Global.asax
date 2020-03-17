f3e13f03f037ce629e4a10d439b1c6ae8e2a9579
linhnkth <43538558+linhnkth@users.noreply.github.com>
2020-03-17 22:16:07 +0700

    examWAD

---
+++ Baithi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Baithi.MvcApplication" Language="C#" %>
---
+++ Baithi/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Baithi.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public int Salary { get; set; }
+    }
+}
